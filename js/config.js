856e64af23fc13b552a6f50f056ccff5331697c1
josesan28 <san24092@uvg.edu.gt>
2026-05-06 16:49:14 -0600

    Configuración añadida para el deploy

---
+++ js/config.js
@@ -0,0 +1,3 @@
+const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+  ? 'http://localhost:8080'
+  : 'proyecto-1-backend-web-production.up.railway.app';
