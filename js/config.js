82625181a7add3f1d5743f15ecda1b0f0a058009
josesan28 <san24092@uvg.edu.gt>
2026-05-06 18:09:40 -0600

    Corrección en ruta

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+var API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://localhost:8080'
   : 'https://proyecto-1-backend-web-production.up.railway.app';
